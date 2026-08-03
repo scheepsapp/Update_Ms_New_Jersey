c0707d8c8233e069a1f294de90b56720113c0eb3
scheepsapp <scheepsapp@gmail.com>
2026-08-03 20:18:03 +0200

    1.2.0

--- update.bat
+++ update.bat
@@ -48,11 +48,12 @@ if exist "%appDir%\waterstanden.html" attrib -h -s -r "%appDir%\waterstanden.htm
 move /y "%TEMP%\waterstanden.html" "%appDir%\waterstanden.html" >nul
 attrib +h +s +r "%appDir%\waterstanden.html"
 
-echo Bezig met hafenmeester_mannheim.html...
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%baseUrl%/hafenmeester_mannheim.html' -OutFile '%TEMP%\hafenmeester_mannheim.html'"
-if exist "%appDir%\hafenmeester_mannheim.html" attrib -h -s -r "%appDir%\hafenmeester_mannheim.html"
-move /y "%TEMP%\hafenmeester_mannheim.html" "%appDir%\hafenmeester_mannheim.html" >nul
-attrib +h +s +r "%appDir%\hafenmeester_mannheim.html"
+echo Bezig met mannheim.html...
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%baseUrl%/mannheim.html' -OutFile '%TEMP%\mannheim.html'"
+if exist "%appDir%\mannheim.html" attrib -h -s -r "%appDir%\mannheim.html"
+move /y "%TEMP%\mannheim.html" "%appDir%\mannheim.html" >nul
+attrib +h +s +r "%appDir%\mannheim.html"
+
 
 echo.
 echo ============================================
